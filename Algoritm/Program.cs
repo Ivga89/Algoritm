4a1f215fb259915b419e984f8558a3ce02e6e102
Ivga89 <borozdinaes89@gmail.com>
2024-12-28 20:40:58 +0500

    ready

--- Algoritm/Program.cs
+++ Algoritm/Program.cs
@@ -1,7 +1,44 @@
-﻿internal class Program
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Algoritm
 {
-    private static void Main(string[] args)
+    internal class Program
     {
-        Console.WriteLine("Hello, World!");
+        private static void Main(string[] args)
+        {
+            List<User> users = new List<User>();
+            User user1 = new User("vasya", "Vasiliy", true);
+            User user2 = new User("zzz00", "Grisha", false);
+            User user3 = new User("lililiX", "Kristina", false);
+            users.Add(user1);
+            users.Add(user2);
+            users.Add(user3);
+
+            foreach(User user in users)
+            {
+                Console.WriteLine($"Hello {user.Name}");
+                if (user.IsPremium == false)
+                {
+                    ShowAds();
+                }
+                Console.WriteLine();
+            }
+
+        }
+
+        static void ShowAds()
+        {
+            Console.WriteLine("Посетите наш новый сайт с бесплатными играми free.games.for.a.fool.com");
+            // Остановка на 1 с
+            Thread.Sleep(1000);
+
+            Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда.");
+            // Остановка на 2 с
+            Thread.Sleep(2000);
+
+            Console.WriteLine("Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу.");
+            // Остановка на 3 с
+            Thread.Sleep(3000);
+        }
     }
 }
